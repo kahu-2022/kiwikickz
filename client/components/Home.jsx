3810bd9972059cf3e60742281d73ab0b121edfa4
Sam Kirby <93824325+samuel-wd-kirby@users.noreply.github.com>
2022-02-24 07:52:33 +1300

    changed action key for ADD_SHOES, fixed dispatch to now be called

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,15 +1,16 @@
 import React, { useEffect } from 'react'
-import { useDispatch ,useSelctor } from 'react-redux'
+import { useDispatch ,useSelector } from 'react-redux'
 import { addAllShoesThunk } from '../actions/shoes'
 
 function Home () {
-  const dispatch = useDispatch
+  
+  const dispatch = useDispatch()
 
   useEffect(() => {
     dispatch(addAllShoesThunk())
   }, [])
 
-  const allShoes = useSelctor(state => state.allShoes)
+  const allShoes = useSelector(state => state.allShoes)
   console.log(allShoes)
 
   return (
--- client/reducers/allShoes.js
+++ client/reducers/allShoes.js
@@ -1,9 +1,9 @@
-import { ADD_SHOES } from '../actions/meetings'
+import { ADD_SHOES } from '../actions/shoes'
 
 function shoes (state = {}, action) {
     switch (action.type) {
         case ADD_SHOES:
-          return action.shoes
+          return action.shoeArr
         default:
             return state
     }
