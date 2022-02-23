3350208c4082061fe4736c8421b09e7fe50701c7
sonnygraystone <89627466+sonnygraystone@users.noreply.github.com>
2022-02-24 10:47:23 +1300

    Added product seeds & migration
    Added product seeds & migration along with images to public folder

--- server/db/migrations/20220223060413_product.js
+++ server/db/migrations/20220223060413_product.js
@@ -1,15 +1,29 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+    return knex.schema.createTable('product', (table) => {
+        table.increments('id').primary()
+        table.integer('seller_id')
+        table.boolean('hot_pick')
+        table.string('status')
+        table.string('name')
+        table.string('details')
+        table.boolean('auth_status')
+        table.string('brand')
+        table.integer('size')
+        table.string('color')
+        table.string('make')
+        table.string('model')
+        table.timestamp('created_at').defaultTo(knex.fn.now())
+        table.integer('year')
+        table.integer('price')
+        table.string('condition')
+        table.string('receipt')
+        table.string('image1')
+        table.string('image2')
+        table.string('image3')
+        table.string('image4')
+    })
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
-};
+exports.down = (knex) => {
+    return knex.schema.dropTable('product')
+}
