c770ec85327cba5412167a1ebee1d9da3ef6d5e9
nikolai-sim <89240062+nikolai-sim@users.noreply.github.com>
2022-02-24 11:20:42 +1300

    clean up home component

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -10,20 +10,11 @@ function Home () {
     dispatch(getAllShoesThunk())
   }, [])
 
-  // const [filterAllShoesArr, setFilterAllShoesArr ] = useState([])
   const [hotPicks, setHotPicks ] = useState(true)
 
-  // All shoes form state
+
   const allShoes = useSelector(state => state.allShoes)
 
-//  setFilterAllShoesArr(allShoes)
-
-  console.log(allShoes)
-
-  // Filter Functionality
-  // I think we should filter and set such state here and then on apply filters we set a global filtered state
-
-  // const hotPicks = allShoesArr.map(shoe => shoe.hot_pick === true)
 
   return (
     <>
