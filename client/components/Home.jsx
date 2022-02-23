04648d7b24dbd648493e9d7205e4bbba9a511d0f
samuel-wd-kirby <samkirby@outlook.com>
2022-02-23 22:07:18 +1300

    added necessary imports to use thunk within the component

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch ,useSelctor } from 'react-redux'
+import { addAllShoesThunk } from '../actions/shoes'
 
 function Home () {
+  const dispatch = useDispatch
+
+  useEffect(() => {
+    dispatch(addAllShoesThunk())
+  }, [])
+
+  const allShoes = useSelctor(state => state.allShoes)
+  console.log(allShoes)
+
   return (
     <>
     <h2>Home</h2>
