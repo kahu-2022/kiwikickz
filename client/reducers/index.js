eca080d1c952622d14de6ec70428543e5e7ab002
Sam Kirby <93824325+samuel-wd-kirby@users.noreply.github.com>
2022-02-24 10:21:26 +1300

    Changed allshoes function name, added shoe reducer to index

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux'
 import allShoes from './allShoes'
+import shoe from './shoe'
+
 
 // import stuff from './stuff'
 
 export default combineReducers({
-  allShoes
+  allShoes,
+  shoe
 })
