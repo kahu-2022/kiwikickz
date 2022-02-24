2d857efe463f157e3af192f6ab998ce7e0349b8c
andreas <benzan81@gmail.com>
2022-02-24 16:15:38 +1300

    change route in App.jsx to send :id

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -13,7 +13,7 @@ function App () {
       <section className="main">
         <Routes>
           <Route path='/' element = {<Home/>}/>
-          <Route path='/product' element = {<Product/>}/>
+          <Route path='/product/:id' element = {<Product/>}/>
           <Route path='/cart' element = {<Cart/>}/>
         </Routes>
       </section>
