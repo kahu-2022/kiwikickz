6c850df8ddda184e33605681e64581eb7f309d68
andreas <benzan81@gmail.com>
2022-02-24 18:40:33 +1300

    shoe images showing up on product page

--- client/components/Product.jsx
+++ client/components/Product.jsx
@@ -1,20 +1,29 @@
-import React, {useState} from 'react';
-import { shallowEqual } from 'react-redux';
+import React, {useState, useEffect } from 'react';
 import { useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
-
-
+import { useDispatch } from 'react-redux'
+import { getAllShoesThunk } from '../actions/shoes'
+import { Card, Grid , Icon, Image , Reveal} from 'semantic-ui-react'
 
 function Product () {
 
  const { id } = useParams()
+ const dispatch = useDispatch()
+ const shoe = useSelector(globalState => globalState.allShoes[id - 1])
 
+  useEffect(() => {
+    dispatch(getAllShoesThunk())
+  }, [])
 
   return (
     <>
     <header />
-    <h2>Products</h2>
     <p>shoe id from URL is: {id}</p>
+    <p>{shoe ? shoe.name : "nothing"}</p>
+    <div>{shoe ? <Image size= 'medium' src={shoe.image1} /> : "nothing"}</div>
+    <div>{shoe ? <Image size= 'medium' src={shoe.image2} /> : "nothing"}</div>
+    <div>{shoe ? <Image size= 'medium' src={shoe.image3} /> : "nothing"}</div>
+    <div>{shoe ? <Image size= 'medium' src={shoe.image4} /> : "nothing"}</div>
     </>
   )
 }
