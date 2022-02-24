ccfa1e6fedb01c36fffdfb63a3afeedc5aa6cd06
nikolai-sim <89240062+nikolai-sim@users.noreply.github.com>
2022-02-24 16:11:42 +1300

    update image folders to correct paths for broken image files

--- client/components/ShoeCard.jsx
+++ client/components/ShoeCard.jsx
@@ -1,28 +1,29 @@
 import React from 'react'
-import { Card, Grid , Icon, Image} from 'semantic-ui-react'
+import { Card, Grid , Icon, Image , Reveal} from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 
 function ShoeCard(props) {
   const obj = props.data
 
   return (
     <Grid.Column>
-  <Card>
-    <Image src={obj.image1} wrapped ui={false} />
-    <Card.Content>
-      <Card.Header>{obj.name}</Card.Header>
-      <Card.Meta>
-        <span className='date'>{obj.createdDate}</span>
-      </Card.Meta>
-      <Card.Description>
-        {`Size : ${obj.size}`}
-      </Card.Description>
-    </Card.Content>
-    <Card.Content extra>
-      <a>
-      <Card.Header>{`$ ${obj.price}`}</Card.Header>
-      </a>
-    </Card.Content>
-  </Card>
+      <Card>
+        <Image src={obj.image1} wrapped ui={false} />
+        <Card.Content>
+          <Card.Header>{obj.name}</Card.Header>
+          <Card.Meta>
+            <span className='date'>{obj.createdDate}</span>
+          </Card.Meta>
+          <Card.Description>
+            {`Size : ${obj.size}`}
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <a>
+          <Card.Header as='h2'>{`$ ${obj.price}`}</Card.Header>
+          </a>
+        </Card.Content>
+      </Card>
 
   </Grid.Column>
   )
