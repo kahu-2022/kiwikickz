a30c639bd215318a5c7bb7817324191949d58e91
nikolai-sim <89240062+nikolai-sim@users.noreply.github.com>
2022-02-25 11:37:39 +1300

    add pop up when added to cart

--- client/components/ProductCard.jsx
+++ client/components/ProductCard.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import { Card, Grid , Icon, Image , Reveal} from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
-import { addToCart } from '../actions/products'
+import AddToCartPopUp from './AddToCartPopUp'
 
 function ProductCard(props) {
   const obj = props.data
-  const dispatch = useDispatch()
+  
 
   return (
     <>
@@ -33,7 +32,7 @@ function ProductCard(props) {
           </Card.Description>
         </Card.Content>
         <Card.Content extra>
-          <Card.Header as='h2'><a onClick={() => {dispatch(addToCart(obj))}}><Icon name='plus circle'/></a>{`$ ${obj.price}`}</Card.Header>
+          <AddToCartPopUp data={obj}/>
           
         </Card.Content>
       </Card>
