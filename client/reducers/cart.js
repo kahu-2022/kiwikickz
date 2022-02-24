2c26d216d71685fd60673a1af6899dbfe343aa20
nikolai-sim <89240062+nikolai-sim@users.noreply.github.com>
2022-02-25 08:10:13 +1300

    add, add item case to cart reducer

---
+++ client/reducers/cart.js
@@ -0,0 +1,10 @@
+const initialState = []
+
+function cart (state = initialState, action) { 
+  switch(action.type) {
+    case 'ADD_ITEM' : 
+      return [...state, action.item]
+    default: 
+      return state
+  }
+}
