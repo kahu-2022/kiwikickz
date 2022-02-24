dcb5f679e514617866f4373273630d162285edb6
Sam Kirby <93824325+samuel-wd-kirby@users.noreply.github.com>
2022-02-24 19:33:08 +1300

    added new product reducer

---
+++ client/reducers/product.js
@@ -0,0 +1,31 @@
+function product (state = {
+    id: 1,
+    seller_id: 1,
+    hot_pick: true,
+    status: 'available',
+    name: 'Jordan 1 Retro High',
+    details: 'Jordan Brand connected with Parisian fashion house.',
+    auth_status: true,
+    brand: 'Nike',
+    size: 9,
+    color: 'White',
+    make: 'Jordan 1',
+    model: 'Dior High',
+    year: 2020,
+    price: 8000,
+    condition: 'New',
+    receipt: '',
+    image1: '/productimages/dior/dior1.jpg',
+    image2: '/productimages/dior/dior2.jpg',
+    image3: '/productimages/dior/dior3.jpg',
+    image4: '/productimages/dior/dior4.jpeg'
+
+}, action) {
+    switch (action.type) {
+
+        default:
+            return state
+    }
+}
+
+export default product
