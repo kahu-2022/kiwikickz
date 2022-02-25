623539feaab734880a5888ec3afc5c5553280f6e
Sam Kirby <93824325+samuel-wd-kirby@users.noreply.github.com>
2022-02-26 12:30:21 +1300

    added lib file and folder

--- client/lib.js
+++ client/lib.js
@@ -1,8 +1,22 @@
-export function mappingWithFilters(ArrayToFilter, Filters) {
+export const mappingWithFilters = (ArrayToFilter, Filters) =>
+  ArrayToFilter.map(product => {
+    const matches = productMatchesAllFilters(product, filters);
+    return { product, mtches }
+  });
+
+const productMatchesAllFilters = (product, filters) =>
+  filters.every(filter => productMatchesFilter(product, filter))
 
 
+const productMatchesFilter = (product, filter) =>
+  product[filter.key] === filter.value;
+
+ export const productFitsFilter = (product, filter) => {
+  if (product[filter.key] === filter.value) {
+    console.log(product)
+    return product
+  }
 }
-
 /* Suggested filters for array :
 
 An example of what the Filters arguement would look like
