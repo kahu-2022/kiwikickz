9e9aaf089d3a249975862920bfd95b2080ea08f4
Sam Kirby <93824325+samuel-wd-kirby@users.noreply.github.com>
2022-02-26 19:31:23 +1300

    conditionally renders now

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getAllProductsThunk } from '../actions/products'
+
 import {Routes, Route, Link} from 'react-router-dom'
 import Cart from './Cart'
 import Home from './Home'
@@ -6,22 +9,36 @@ import Product from './Product'
 import { Header } from 'semantic-ui-react'
 import AddProduct from './AddProduct'
 
-function App () {
+function App() {
+  
+  const isProductArrFull = useSelector(state => state.isProductArrFull)
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getAllProductsThunk())
+  }, [])
+
   return (
     <>
-      <header className="header">
-      <header className="header">
-        <Header as = 'h1' textAlign='center'>KIWIKICKZ</Header>
-      </header>
-      </header>
-      <section className="main">
-        <Routes>
-          <Route path='/' element = {<Home/>}/>
-          <Route path='/product/:id' element = {<Product/>}/>
-          <Route path='/cart' element = {<Cart/>}/>
-          <Route path='/addproduct' element = {<AddProduct/>}/>
-        </Routes>
-      </section>
+      {isProductArrFull
+        ? <>
+          <header className="header">
+            <header className="header">
+              <Header as='h1' textAlign='center'>KIWIKICKZ</Header>
+            </header>
+          </header>
+          <section className="main">
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/product/:id' element={<Product />} />
+              <Route path='/cart' element={<Cart />} />
+              <Route path='/addproduct' element={<AddProduct />} />
+            </Routes>
+          </section>
+        </>
+        : null
+      }
     </>
   )
 }
