04130a79db93f6d311839f5887098741088270e7
mei-yang <93880566+meiyangdev@users.noreply.github.com>
2022-02-26 15:35:46 +1300

    make offer done

--- client/components/MakeOffer.jsx
+++ client/components/MakeOffer.jsx
@@ -1,25 +1,44 @@
-import React from "react";
-import { Container, Button } from 'semantic-ui-react'
+import React, {useState} from "react";
+import { Container, Button, Input, Label } from 'semantic-ui-react'
 import { addToCart } from '../actions/products'
 import { useDispatch } from 'react-redux' 
 import { useNavigate } from 'react-router-dom'
-
+import  TextPopUp  from "./TextPopUp";
 function MakeOffer(props) {
   const product = props.data
   const navigate = useNavigate()
   const dispatch = useDispatch()
+  const [offer, setOffer] = useState(false)
 
   const handleBuyNow = () => {
     dispatch(addToCart(product))
     navigate('/cart')
   }
 
+  const toggleOffer=() =>{
+    return offer ? setOffer(false) : setOffer(true)
+  }
+  
+  const dropOffer=() => {
+   return (
+     <>
+    <Input labelPosition='right' type='text' placeholder='Amount'>
+    <Label basic>$</Label>
+    <input />
+    <Label>.00</Label>
+    </Input>
+    <TextPopUp text={'Offer Sent.'} button = {<Button>Submit</Button>}/>
+    </>
+   ) 
+  }
+
 return (
   <>
   <Container>
   <Button onClick={handleBuyNow}>Buy Now</Button>
-  <Button>Make Offer</Button>
-  
+  <Button onClick = {toggleOffer} >Make Offer</Button>
+  {offer? dropOffer() : null}
+
   </Container>
   </>
 )
