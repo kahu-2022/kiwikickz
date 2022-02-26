4887e3079d886ad1200eaac32896905fe0635661
Sam Kirby <93824325+samuel-wd-kirby@users.noreply.github.com>
2022-02-26 19:30:46 +1300

    added new reducer to index

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import allProducts from './allProducts'
 import product from './product'
 import cart from './cart'
+import isProductArrFull from './isThereShitInTheProductState'
 
 export default combineReducers({
   allProducts,
   product,
-  cart
+  cart,
+  isProductArrFull
 })
