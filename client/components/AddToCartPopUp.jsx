f3014df5b977215049fe87636e5df4b49e0327e7
nikolai-sim <nikolai_sim@hotmail.com>
2022-02-27 19:14:42 +1300

    add remove from cart conditional functionality

--- client/components/AddToCartPopUp.jsx
+++ client/components/AddToCartPopUp.jsx
@@ -1,25 +1,38 @@
 import React from 'react'
 import { Card, Popup , Icon} from 'semantic-ui-react'
 import { useDispatch } from 'react-redux'
-import { addToCart } from '../actions/products'
+import { addToCart , removeFromCart} from '../actions/products'
+import { useSelector } from 'react-redux'
+
 
 
 
 function AddToCartPopUp (props) {
 
+  const cart = useSelector( state => state.cart)
   const product = props.data
   const dispatch = useDispatch()
+  let inCart
+
+  if (cart.filter(function(e) { return e.id === product.id; }).length > 0) {
+    inCart = false
+  }
+  else {
+    inCart = true
+  }
 
   return(
   <Popup
     content={
       <>
-        Added to cart. 
+      {inCart ? 'Removed from Cart' : 'Added to cart.'}
       </>
     }
     on='click'
     popper={{ id: 'popper-container', style: { zIndex: 2000 } }}
-    trigger={<Card.Header as='h2'><a onClick={() => {dispatch(addToCart(product))}}><Icon name='plus circle'/></a>{`$ ${product.price}`}</Card.Header>}
+    trigger={<Card.Header as='h2'>
+      {inCart ?
+      <a onClick={() => {dispatch(addToCart(product))}}><Icon name='plus circle'/></a> : <a onClick={() => {dispatch(removeFromCart(product))}}><Icon name='minus circle'/></a> }{`$ ${product.price}`}</Card.Header>}
     hideOnScroll
   />
 )}
