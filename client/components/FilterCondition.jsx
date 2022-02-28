75a8805d24b80959d125edd6e911f8640e224825
Sam Kirby <93824325+samuel-wd-kirby@users.noreply.github.com>
2022-02-28 19:43:15 +1300

    Added onclick component to FilterCondition

--- client/components/FilterCondition.jsx
+++ client/components/FilterCondition.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
-import { Grid , Container, Checkbox, GridRow, GridColumn, Header, Radio } from 'semantic-ui-react'
+import { Grid, Container, Checkbox, GridRow, GridColumn, Header, Radio } from 'semantic-ui-react'
+import { filters } from '../actions/products'
+
 
 function FilterCondition() {
   const dispatch = useDispatch()
