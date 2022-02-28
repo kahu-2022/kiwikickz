d0beb315a5d6f602815163c3c977af5e46b08475
Sam Kirby <93824325+samuel-wd-kirby@users.noreply.github.com>
2022-02-28 19:43:54 +1300

    Added onclick feature tofilterprice

--- client/components/FilterPrice.jsx
+++ client/components/FilterPrice.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
-import { Grid , Container, Checkbox, GridRow, GridColumn, Header, Radio } from 'semantic-ui-react'
+import { Grid, Container, Checkbox, GridRow, GridColumn, Header, Radio } from 'semantic-ui-react'
+import { filters } from '../actions/products'
+
 
 function FilterPrice() {
   const dispatch = useDispatch()
@@ -17,13 +19,13 @@ function FilterPrice() {
         <Checkbox onClick={() => { dispatch(filters({ key: 'price', min: 100, max: 150 })) }}/> {`$100 - $150`}
       </GridColumn>
       <GridColumn>
-        <Checkbox onClick={() => { dispatch(filters({ key: 'price', min: 0, max: 100 })) }}/> {`$150 - 200`}
+        <Checkbox onClick={() => { dispatch(filters({ key: 'price', min: 150, max: 200 })) }}/> {`$150 - 200`}
       </GridColumn>
       <GridColumn>
-        <Checkbox onClick={() => { dispatch(filters({ key: 'price', min: 0, max: 100 })) }}/> {`$200 - $250`}
+        <Checkbox onClick={() => { dispatch(filters({ key: 'price', min: 200, max: 250 })) }}/> {`$200 - $250`}
       </GridColumn>
       <GridColumn>
-        <Checkbox onClick={() => { dispatch(filters({ key: 'price', min: 0, max: 100 })) }}/> {`$300 & Over`}
+        <Checkbox onClick={() => { dispatch(filters({ key: 'price', min: 300, max: 9999 })) }}/> {`$300 & Over`}
       </GridColumn>
 
     </Grid>
