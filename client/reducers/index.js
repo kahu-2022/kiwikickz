5d154b241eff47d6556604b30bb0da8602ee04a2
Sam Kirby <93824325+samuel-wd-kirby@users.noreply.github.com>
2022-02-28 15:40:46 +1300

    added new reducer to reducer index

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,13 +2,13 @@ import { combineReducers } from 'redux'
 import allProducts from './allProducts'
 import product from './product'
 import cart from './cart'
-import isProductArrFull from './isThereShitInTheProductState'
 import allQuestions from './allQuestions'
+import currentFilters from "./currentFilters";
 
 export default combineReducers({
   allProducts,
   product,
   cart,
-  isProductArrFull,
-  allQuestions
+  allQuestions,
+  currentFilters
 })
