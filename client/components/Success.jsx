9b27f95122d6ec1c81c77d3b7c8d052f7851dbd2
sonnygraystone <89627466+sonnygraystone@users.noreply.github.com>
2022-02-28 20:08:40 +1300

    navigate to success on res code 200 OKAY
    navigate to success on res code 200 working

--- client/components/Success.jsx
+++ client/components/Success.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
-import { useLocation } from 'react-router-dom'
 
 export default function Success() {
-    const location = useLocation
-
-    console.log(location)
     return (
         <div>
             Successful
