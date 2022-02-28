3b09b91ff90073fb8b94050ceaf53a8f7d508190
Sam Kirby <93824325+samuel-wd-kirby@users.noreply.github.com>
2022-02-28 15:41:24 +1300

    able to check a product matches all filters and also add and remove filters from filter reducer

--- client/lib.js
+++ client/lib.js
@@ -1,30 +1,55 @@
-export const mappingWithFilters = (ArrayToFilter, Filters) =>
-  ArrayToFilter.map(product => {
-    const matches = productMatchesAllFilters(product, filters);
-    return { product, mtches }
-  });
+// export const mappingWithFilters = (ArrayToFilter, Filters) =>
+//   ArrayToFilter.map(product => {
+//     const matches = productMatchesAllFilters(product, filters);
+//     return { product, mtches }
+//   });
 
-const productMatchesAllFilters = (product, filters) =>
-  filters.every(filter => productMatchesFilter(product, filter))
-
-
-const productMatchesFilter = (product, filter) =>
-  product[filter.key] === filter.value;
+// const productMatchesAllFilters = (product, filters) =>
+//   filters.every(filter => productMatchesFilter(product, filter))
 
 
+// const productMatchesFilter = (product, filter) =>
+//   product[filter.key] === filter.value;
 
 
-export const manyProductsFitsFilter = (products, filter) => {
-  const please = products.filter(product => productFitsFilter(product, filter))
-  return please
+export const manyProductsManyFilters = (products, filters) => {
+  const results = products.map(product => manyFiltersOneProduct(product, filters))
+  const finalResults = results.filter(product => product !== null)
+  return finalResults
+}
+
+const manyFiltersOneProduct = (product, filters) => {
+  const potentialPass = filters.every(filter => productFitsFilter(product, filter))
+
+  if (potentialPass === true) {
+    return product
+  }
+  else return null
 }
   
   
 const productFitsFilter = (product, filter) => {
-  if (product[filter.key] === filter.value) {
+  if (product[filter.key] === 'color') {
+    const colorTest = product[filter.key].some(color => color === filter.value)
+    if (colorTest === true) {
+      return product
+    }
+  }
+  else if (product[filter.key] === filter.value) {
     return product
   }
 }
+
+
+export const testingFilterArr = (arrToTest, filter) => {
+  const isInArray = arrToTest.some(product => product.key === filter.key)
+  if (isInArray === true) {
+    return arrToTest.filter(product => product.key !== filter.key)
+  }
+  else return [...arrToTest, filter]
+}
+
+
 /* Suggested filters for array :
 
 An example of what the Filters arguement would look like
