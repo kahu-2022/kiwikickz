ad55ebbdd2c25c4658f523c943ca3886eed6f3f0
andreas <benzan81@gmail.com>
2022-03-01 15:02:14 +1300

    remove fetch all questions button

--- client/components/QuestionAdmin.jsx
+++ client/components/QuestionAdmin.jsx
@@ -73,7 +73,6 @@ function QuestionAdmin() {
         </>
         )}
         </ul>
-        <button onClick = {(e) => dispatch(updateQuestionThunk(updateQuestion))}>Update Question</button>
         
       <h2>Answered Questions</h2>
       <ul>
