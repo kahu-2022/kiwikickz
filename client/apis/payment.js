bee09374a6a3b670002e7627c3d0a58aef70164b
Kelly Keating <keating.kel@gmail.com>
2022-03-02 09:31:16 +1300

    remove unnecessary comments

--- client/apis/payment.js
+++ client/apis/payment.js
@@ -3,9 +3,6 @@ import request from "superagent"
 
 
 const BASE_URL = "http://localhost:3000";
-// const TOKEN =
-//   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
-//     .accessToken || "";
 
 const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
 const currentUser = user && JSON.parse(user).currentUser;
