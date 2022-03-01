a7c971d7b5ec9552cc2febd4e64514982f68916b
Kelly Keating <keating.kel@gmail.com>
2022-03-02 09:33:34 +1300

    prop types used?

--- client/components/CustomDotGroup.jsx
+++ client/components/CustomDotGroup.jsx
@@ -1,9 +1,12 @@
+
+// TODO: do you want this cause it's nowhere else?
 import PropTypes from "prop-types";
+
 import { Dot } from "pure-react-carousel";
 import React from "react";
 import { Button, Container } from "semantic-ui-react";
 
-const CustomDotGroup = ({ slides, size }) => (
+const CustomDotGroup = ({ slides, size = 'mini' }) => (
   <Container textAlign="center">
     <Button.Group size={size}>
       {[...Array(slides).keys()].map(slide => (
@@ -13,13 +16,13 @@ const CustomDotGroup = ({ slides, size }) => (
   </Container>
 );
 
-CustomDotGroup.defaultProps = {
-  size: "mini"
-};
+// CustomDotGroup.defaultProps = {
+//   size: "mini"
+// };
 
-CustomDotGroup.propTypes = {
-  slides: PropTypes.number.isRequired,
-  size: PropTypes.string
-};
+// CustomDotGroup.propTypes = {
+//   slides: PropTypes.number.isRequired,
+//   size: PropTypes.string
+// };
 
 export default CustomDotGroup;
