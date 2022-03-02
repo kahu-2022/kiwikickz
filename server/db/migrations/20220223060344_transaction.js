3f766da3cd36e5293c38e926fdcb046a6003b007
nikolai-sim <nikolai_sim@hotmail.com>
2022-03-02 13:38:24 +1300

    change int to integer

--- server/db/migrations/20220223060344_transaction.js
+++ server/db/migrations/20220223060344_transaction.js
@@ -2,7 +2,7 @@ exports.up = function (knex) {
   return knex.schema.createTable('transaction', table => {
     table.increments('id')
     table.string('products_purchased')
-    table.int('transaction_amount')
+    table.integer('transaction_amount')
     table.string('buyer_email')
     table.timestamp('created_date').defaultTo(knex.fn.now())
   })
