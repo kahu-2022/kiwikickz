f56cf8ee68e1cf20e770e1116cb2b39e31c4133f
nikolai-sim <nikolai_sim@hotmail.com>
2022-03-02 13:42:04 +1300

    remove chart from homejsx

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getAllProductsThunk } from '../actions/products'
 import { Grid, Container, Divider } from 'semantic-ui-react'
-import Graph from './Chart'
 
 import ProductContainer from './ProductContainer'
 
