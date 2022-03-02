b985b8c5b1624cc4b3ac971bacc3d95927d9265f
nikolai-sim <nikolai_sim@hotmail.com>
2022-03-02 14:25:41 +1300

    remove questions from sold products

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -2,38 +2,27 @@ import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getAllProductsThunk } from '../actions/products'
 import { Grid, Container, Divider } from 'semantic-ui-react'
-import Graph from './Chart'
+
 
 import ProductContainer from './ProductContainer'
 
 import { manyProductsManyFilters } from '../lib'
 
-import { filters } from '../actions/products'
 
 function Home() {
 
-  const dispatch = useDispatch()
 
   const allProducts = useSelector(state => state.allProducts)
   const currentFilters = useSelector(state => state.currentFilters)
 
-  const [sortedProducts, setSortedProducts] = useState('')
 
   const testFilter = { key: 'price', min: 230, max: 270 }
 
   const results = manyProductsManyFilters(allProducts, currentFilters)
 
-  // console.log(results)
-  // console.log('currentFilters', currentFilters)
-
-
   return (
     <>
-      {/* <select name="choice">
-        <option value="first">First Value</option>
-        <option value="second" selected>Second Value</option>
-        <option value="third">Third Value</option>
-      </select> */}
+    
       <Container style={{ marginTop: '3em' }}>
 
         {allProducts ? <ProductContainer data={results} /> : null}
